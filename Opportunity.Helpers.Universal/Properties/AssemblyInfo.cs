d3f37a72f9036b0d5c6a201f61795f150092ca99
OpportunityLiu <liuzhengyuan13@outlook.com>
2017-09-17 10:29:28 +0800

    add info

--- Opportunity.Helpers.Universal/Properties/AssemblyInfo.cs
+++ Opportunity.Helpers.Universal/Properties/AssemblyInfo.cs
@@ -6,23 +6,17 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Opportunity.Helpers.Universal")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Opportunity.Helpers.Universal")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.4.0")]
+[assembly: AssemblyDescription("Helper for UWP apps and libs")]
+[assembly: AssemblyCompany("Opportunity")]
+[assembly: AssemblyProduct("Opportunity.Helpers")]
+[assembly: AssemblyCopyright("Copyright © Opportunity 2017")]
+[assembly: AssemblyTrademark("Opportunity")]
+[assembly: AssemblyVersion(AssemblyInfo.Version)]
+[assembly: AssemblyFileVersion(AssemblyInfo.Version)]
 [assembly: ComVisible(false)]
+//[assembly: InternalsVisibleTo("Opportunity.MvvmUniverse.Test")]
+
+internal static class AssemblyInfo
+{
+    public const string Version = "1.0.4.0";
+}
