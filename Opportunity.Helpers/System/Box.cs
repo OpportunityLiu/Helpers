9cc5c7703f14379a7cb64cca4537f96606708846
OpportunityLiu <Opportunity@live.in>
2017-12-26 14:59:01 +0800

    box

---
+++ Opportunity.Helpers/System/Box.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Opportunity.System
+{
+    /// <summary>
+    /// Simple box of value.
+    /// </summary>
+    /// <typeparam name="T">Value type</typeparam>
+    public class Box<T> : IBox
+    {
+        /// <summary>
+        /// Value in the <see cref="Box{T}"/>
+        /// </summary>
+        public T Value { get; set; }
+        object IBox.Value
+        {
+            get => Value;
+            set => Value = (T)value;
+        }
+    }
+
+    /// <summary>
+    /// Simple box of value.
+    /// </summary>
+    public interface IBox
+    {
+        /// <summary>
+        /// Value in the <see cref="IBox"/>
+        /// </summary>
+        object Value { get; set; }
+    }
+}
