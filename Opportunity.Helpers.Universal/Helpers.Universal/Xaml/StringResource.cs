a594ff64fa24ca5018d75ccc1de28b4fa237c5c7
OpportunityLiu <opportunity@live.in>
2018-03-09 16:06:43 +0800

    markup extension

---
+++ Opportunity.Helpers.Universal/Helpers.Universal/Xaml/StringResource.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Markup;
+
+namespace Opportunity.Helpers.Universal.Xaml
+{
+    public sealed class StringResource : MarkupExtension
+    {
+        public StringResource(string path) { this.Path = path; }
+        public StringResource() { }
+
+        public string Path { get; set; }
+
+        protected override object ProvideValue()
+        {
+            var r = LocalizedStrings.GetValue(Path);
+            if (string.IsNullOrEmpty(r))
+                return Path;
+            return r;
+        }
+    }
+}
